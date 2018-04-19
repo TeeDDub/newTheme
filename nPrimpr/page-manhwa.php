b3dd01b95ac11a362495446175be165f9da88191
이민혁 <min@Mins-MacBook-Pro.local>
2018-04-19 16:20:19 +0900

    wow

---
+++ nPrimpr/page-manhwa.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <div>
+
+        </div>
+        <div class="registerBox">
+            <a class="registerLink">
+                1. 슈퍼히어로 만화사 - 김닛코
+            </a>
+            <a class="registerLink">
+                2. 일본 만화와 현대의 삶 - 정지우
+            </a>
+            <a class="registerLink">
+                3. 예술의 해방과 대중만화 - 성인수
+            </a>
+            <a class="registerLink">
+                4. 우리는 왜 만화를 혐오하는가 - 강상준
+            </a>
+            <a class="registerLink">
+                5. - 박석환
+            </a>
+        </div>
+    </main>
+  </div>
+
+<?php get_footer(); ?>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Black+Han+Sans');
+
+.registerBox{
+    width: 100%;
+
+}
+.registerLink {
+    width: 100%;
+    height: 12%;
+    display: block;
+    background: RGB(070,163,210);
+    font-family: 'Black Han Sans', sans-serif;
+    font-size: 1.25em;
+    text-align: center;
+    text-wrap: unrestricted;
+    cursor: pointer;
+}
+</style>
